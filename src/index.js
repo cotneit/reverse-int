4b82735ae2dba690271ec31767356bd04db400a0
Vladyslav Karasov <36513243+cotneit@users.noreply.github.com>
2020-10-11 17:55:50 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+  let result = '';
+
+  n.toString().split('').forEach((current) => {
+    result = current + result;
+  })
+
+  result = result[result.length - 1] === '-' ? result.slice(0, -1) : result
+
+  return result;
 }
